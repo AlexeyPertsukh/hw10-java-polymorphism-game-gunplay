7943268cbbaad08036bfaaca8329fe9f2513bd40
Alexey Pertsukh <85017966+AlexeyPertsukh@users.noreply.github.com>
2022-08-17 20:10:18 +0300

    change chance bin Shotgun

--- src/hw10_2_game_gunplay/gun/Shotgun.java
+++ src/hw10_2_game_gunplay/gun/Shotgun.java
@@ -3,9 +3,9 @@ package hw10_2_game_gunplay.gun;
 public class Shotgun extends Gun {
     private static final String NAME = "Обрез";
     private static final int DAMAGE_MIN = 30;
-    private static final int DAMAGE_MAX = 45;
+    private static final int DAMAGE_MAX = 50;
     private static final int CARTRIDGE = 10;
-    private static final int CHANCE = 50;
+    private static final int CHANCE = 40;
 
     public Shotgun() {
         super(NAME, DAMAGE_MIN, DAMAGE_MAX, CARTRIDGE, CHANCE);
